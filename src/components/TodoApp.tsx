92c86051da79566c5864788987b74cf1f24d01a6
tuankiet1709 <tranhatuankiet1709@gmail.com>
2023-06-21 06:22:23 +0700

    add state to TodoApp

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -2,10 +2,34 @@ import React from 'react';
 import Header from './layout/Header';
 
 class TodoApp extends React.Component {
+	state = {
+		todos: [
+			{
+				id: 1,
+				title: 'Setup development environment',
+				completed: true,
+			},
+			{
+				id: 2,
+				title: 'Develop website and add content',
+				completed: false,
+			},
+			{
+				id: 3,
+				title: 'Deploy to live server',
+				completed: false,
+			},
+		],
+	};
 	render() {
 		return (
 			<div>
 				<Header />
+				<ul>
+					{this.state.todos.map((todo) => (
+						<li key={todo.id}>{todo.title}</li>
+					))}
+				</ul>
 			</div>
 		);
 	}
