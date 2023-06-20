acf439e9bdfb98e6de3d42bbe9a1e05c9999baec
tuankiet1709 <tranhatuankiet1709@gmail.com>
2023-06-21 06:13:46 +0700

    add Header and create structure for project

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TodoApp from './components/TodoApp';
+import './App.css';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<TodoApp />
 	</React.StrictMode>
 );
 
